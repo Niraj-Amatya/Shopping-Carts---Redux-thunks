2f2ee1f199959e8bdad804ee238a2d4b3916c7ba
Niraj <amatyaraj@gmail.com>
2022-08-23 12:05:05 +1000

    dispatch action when cart button is clicked
    -action is dispatched on the button clicked event.
    -this triggers the reducer which updates the state.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../Store/UI-Slice';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(uiActions.toggleCartButton());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleClick}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>

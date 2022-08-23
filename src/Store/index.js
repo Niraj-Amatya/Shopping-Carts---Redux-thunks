bfcaddcc807af7f999b77351554d16a067296a9e
Niraj <amatyaraj@gmail.com>
2022-08-24 09:01:06 +1000

    import cart reducer and configure to the store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import uiSlice from './UI-Slice';
-
+import cartSlice from './Cart-slice';
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;

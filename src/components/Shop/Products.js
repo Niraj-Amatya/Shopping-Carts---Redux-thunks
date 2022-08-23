df15c00381692684bdbf27a8b3728abc56564771
Niraj <amatyaraj@gmail.com>
2022-08-24 09:00:09 +1000

    dummy data for different items created and passed as props to the ProductItem Component

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,45 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const products = [
+  {
+    id: 'a1',
+    title: 'Test1',
+    price: 12,
+    quantity: 1,
+    description: 'This is a first product - amazing!',
+  },
+  {
+    id: 'a2',
+    title: 'Test2',
+    price: 22,
+    quantity: 1,
+    description: 'This is a second product - amazing!',
+  },
+  {
+    id: 'a3',
+    title: 'Test3',
+    price: 40,
+    description: 'This is a last product - amazing!',
+    quantity: 1,
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {products.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+            quantity={item.quantity}
+          />
+        ))}
       </ul>
     </section>
   );
